2edd664f4ec9e8b253b909bc4f289327b4365646
Tpalmer111 <mr.palmer111@gmail.com>
2022-10-10 10:56:04 -0600

    edited comments.js

--- controllers/api-v1/comments.js
+++ controllers/api-v1/comments.js
@@ -6,11 +6,9 @@ const db = require('../../models')
 router.get('/', (req, res) => {
     try {
         res.json({ msg: 'welcome to the comments endpoint' })
-
     } catch(err) {
         console.warn(err)
     }
-
 })
 
 module.exports = router
